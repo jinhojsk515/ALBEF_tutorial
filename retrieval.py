7418f60dcbf40d85c5434f4f52a9d5095871eed9
jinhojsk515 <59189526+jinhojsk515@users.noreply.github.com>
2023-03-08 23:58:43 +0900

    Update retrieval.py

--- retrieval.py
+++ retrieval.py
@@ -1,5 +1,4 @@
 import argparse
-from tqdm import tqdm
 from pathlib import Path
 import utils
 import torch
@@ -7,7 +6,6 @@ import numpy as np
 import random
 from pretrain_albef import ALBEF
 import time
-import os
 import torch.backends.cudnn as cudnn
 from transformers import BertTokenizer
 import datetime
@@ -15,18 +13,17 @@ from torchvision import transforms
 import torchvision.transforms as T
 from dataset import MIMIC_CXRDataset
 from torch.utils.data import DataLoader
-import torch.optim as optim
-from scheduler import create_scheduler
-from sklearn.metrics import f1_score
+from vit import interpolate_pos_embed
 import torch.nn.functional as F
 
 # ALBEF's strategy for retrieval task(ex: Image->Text)
 # When an input image is given, the model choose 'k-test' number of texts for that image,
 # according to their cosine similarity of their contrastive-loss embedding vectors.(this removes a lot of irrelavant images)
 # Then, the model takes those 'k-test' texts and the input image to perform ITM.
-# 'k-test' texts are ranked according to their ITM probability of "matched", and become the retrieval output.
+# 'k-test' texts are ranked according to their ITM probability of being "matched", and become the retrieval output.
 
 # For this code, "Correct retrieval" is defined as "the image/text with the same diagnosis labels"
+
 
 @torch.no_grad()
 def evaluate(model, data_loader, tokenizer, device, config):
@@ -35,7 +32,6 @@ def evaluate(model, data_loader, tokenizer, device, config):
     print('Computing features for evaluation...')
     start_time = time.time()
 
-    #texts = data_loader.dataset.text
     texts=[l['text'] for l in data_loader.dataset.data]
     num_text = len(texts)
     text_bs = 256
@@ -44,10 +40,9 @@ def evaluate(model, data_loader, tokenizer, device, config):
     text_atts = []
     for i in range(0, num_text, text_bs):
         text = texts[i: min(num_text, i + text_bs)]
-        text_input = tokenizer(text, padding='max_length', truncation=True, max_length=150, return_tensors="pt").to(
+        text_input = tokenizer(text, padding='max_length', truncation=True, max_length=200, return_tensors="pt").to(
             device)
-        text_output = model.text_encoder(text_input.input_ids, attention_mask=text_input.attention_mask, mode='text')
-        text_feat = text_output.last_hidden_state
+        text_feat = model.text_encoder.bert(text_input.input_ids, attention_mask=text_input.attention_mask, mode='text').last_hidden_state
         text_embed = F.normalize(model.text_proj(text_feat[:, 0, :]))
         text_embeds.append(text_embed)
         text_feats.append(text_feat)
@@ -59,9 +54,9 @@ def evaluate(model, data_loader, tokenizer, device, config):
     image_feats = []
     image_embeds = []
     labels=[]       #contains the labels(=the criteria for correct retrieval)
-    for image,_, label in data_loader:
+    for image, _, label in data_loader:
         image = image.to(device)
-        image_feat = model.visual_encoder(image,return_dict=True).last_hidden_state[:, 0, :]
+        image_feat = model.visual_encoder(image)[:, 0, :]
         image_embed = model.vision_proj(image_feat)
         image_embed = F.normalize(image_embed, dim=-1)
 
@@ -87,21 +82,20 @@ def evaluate(model, data_loader, tokenizer, device, config):
         topk_sim, topk_idx = sims.topk(k=config['k_test'], dim=0)
         encoder_output = image_feats[start + i].repeat(config['k_test'], 1, 1)
         encoder_att = torch.ones(encoder_output.size()[:-1], dtype=torch.long).to(device)
-        print('kkk',text_feats[topk_idx].size())
-        output_image = model.text_encoder(encoder_embeds = encoder_output,
-                                        attention_mask = encoder_att,
-                                        encoder_hidden_states = text_feats[topk_idx],
-                                        encoder_attention_mask = text_atts[topk_idx],
-                                        return_dict = True,
-                                        mode = 'fusion',
-                                       ).last_hidden_state[:, 0, :]
-        output_text = model.text_encoder(encoder_embeds=text_feats[topk_idx],
-                                    attention_mask=text_atts[topk_idx],
-                                    encoder_hidden_states=encoder_output,
-                                    encoder_attention_mask=encoder_att,
-                                    return_dict=True,
-                                    mode='fusion'
-                                    ).last_hidden_state[:, 0, :]
+        output_image = model.text_encoder.bert(encoder_embeds=encoder_output,
+                                          attention_mask=encoder_att,
+                                          encoder_hidden_states=text_feats[topk_idx],
+                                          encoder_attention_mask=text_atts[topk_idx],
+                                          return_dict=True,
+                                          mode='fusion',
+                                          ).last_hidden_state[:, 0, :]
+        output_text = model.text_encoder.bert(encoder_embeds=text_feats[topk_idx],
+                                         attention_mask=text_atts[topk_idx],
+                                         encoder_hidden_states=encoder_output,
+                                         encoder_attention_mask=encoder_att,
+                                         return_dict=True,
+                                         mode='fusion'
+                                         ).last_hidden_state[:, 0, :]
         output = torch.cat([output_image, output_text], dim=-1)
         score = model.itm_head(output)[:, 1]
         score_matrix_i2t[start + i, topk_idx] = score
@@ -117,20 +111,20 @@ def evaluate(model, data_loader, tokenizer, device, config):
         topk_sim, topk_idx = sims.topk(k=config['k_test'], dim=0)
         encoder_output = image_feats[topk_idx].unsqueeze(1)
         encoder_att = torch.ones(encoder_output.size()[:-1], dtype=torch.long).to(device)
-        output_image = model.text_encoder(encoder_embeds = encoder_output,
-                                        attention_mask = encoder_att,
-                                        encoder_hidden_states = text_feats[start + i].repeat(config['k_test'], 1, 1),
-                                        encoder_attention_mask = text_atts[start + i].repeat(config['k_test'], 1),
-                                        return_dict = True,
-                                        mode = 'fusion',
-                                       ).last_hidden_state[:, 0, :]
-        output_text = model.text_encoder(encoder_embeds=text_feats[start + i].repeat(config['k_test'], 1, 1),
-                                    attention_mask=text_atts[start + i].repeat(config['k_test'], 1),
-                                    encoder_hidden_states=encoder_output,
-                                    encoder_attention_mask=encoder_att,
-                                    return_dict=True,
-                                    mode='fusion'
-                                    ).last_hidden_state[:, 0, :]
+        output_image = model.text_encoder.bert(encoder_embeds=encoder_output,
+                                          attention_mask=encoder_att,
+                                          encoder_hidden_states=text_feats[start + i].repeat(config['k_test'], 1, 1),
+                                          encoder_attention_mask=text_atts[start + i].repeat(config['k_test'], 1),
+                                          return_dict=True,
+                                          mode='fusion',
+                                          ).last_hidden_state[:, 0, :]
+        output_text = model.text_encoder.bert(encoder_embeds=text_feats[start + i].repeat(config['k_test'], 1, 1),
+                                         attention_mask=text_atts[start + i].repeat(config['k_test'], 1),
+                                         encoder_hidden_states=encoder_output,
+                                         encoder_attention_mask=encoder_att,
+                                         return_dict=True,
+                                         mode='fusion'
+                                         ).last_hidden_state[:, 0, :]
         output = torch.cat([output_image, output_text], dim=-1)
         score = model.itm_head(output)[:, 1]
         score_matrix_t2i[start + i, topk_idx] = score
@@ -139,10 +133,11 @@ def evaluate(model, data_loader, tokenizer, device, config):
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print('Evaluation time {}'.format(total_time_str))
 
-    return score_matrix_i2t.cpu().numpy(), score_matrix_t2i.cpu().numpy(),labels
+    return score_matrix_i2t.cpu().numpy(), score_matrix_t2i.cpu().numpy(), labels
+
 
 @torch.no_grad()
-def itm_eval(scores_i2t, scores_t2i,labels):
+def itm_eval(scores_i2t, scores_t2i, labels):
     # Images->Text
     ranks = np.zeros(scores_i2t.shape[0])
     for index, score in enumerate(scores_i2t):
@@ -167,7 +162,7 @@ def itm_eval(scores_i2t, scores_t2i,labels):
             if torch.sum((labels[i]-labels[index])**2)==0:
                 ranks[index]=num
                 break
-    print('T2I MRR:',np.mean(1/(ranks+1)))
+    print('T2I MRR:', np.mean(1/(ranks+1)))
 
     # Compute metrics
     ir1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
@@ -187,8 +182,7 @@ def itm_eval(scores_i2t, scores_t2i,labels):
                    't2i_h10': ir10,
                    't2i_h_mean': ir_mean,
                    'mean': r_mean}
-    print('eval result:',eval_result)
-    return eval_result
+    print('eval result:', eval_result)
 
 
 def main(args, config):
@@ -207,20 +201,18 @@ def main(args, config):
     print("Creating dataset")
     normalize = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     test_transform = transforms.Compose([
-            transforms.Resize((config['image_res'],config['image_res']),interpolation=T.InterpolationMode.BICUBIC),
+            transforms.Resize((config['image_res'],config['image_res']), interpolation=T.InterpolationMode.BICUBIC),
             transforms.ToTensor(),
             normalize,
         ])
-    dataset_val = MIMIC_CXRDataset("./data/MIMIC_CXR/Train.jsonl", transform=test_transform,data_length=[0,100],shuffle=False)
-    dataset_test = MIMIC_CXRDataset("./data/MIMIC_CXR/Test.jsonl", transform=test_transform,data_length=[0,100],shuffle=False)
-    val_loader = DataLoader(dataset_val, batch_size=config['batch_size_test'], pin_memory=True, drop_last=False)
+    dataset_test = MIMIC_CXRDataset("./data/MIMIC_CXR/Test.jsonl", transform=test_transform, data_length=100)
     test_loader = DataLoader(dataset_test, batch_size=config['batch_size_test'], pin_memory=True, drop_last=False)
 
     tokenizer = BertTokenizer.from_pretrained(args.text_encoder)
 
     #### Model ####
     print("Creating model")
-    model = ALBEF(config=config, text_encoder=args.text_encoder, tokenizer=tokenizer)
+    model = ALBEF(config=config, tokenizer=tokenizer)
 
     if args.checkpoint:
         print('LOADING PRETRAINED MODEL..')
@@ -230,16 +222,8 @@ def main(args, config):
         # reshape positional embedding to accomodate for image resolution change
         pos_embed_reshaped = interpolate_pos_embed(state_dict['visual_encoder.pos_embed'], model.visual_encoder)
         state_dict['visual_encoder.pos_embed'] = pos_embed_reshaped
-
-        for key in list(state_dict.keys()):
-            if 'bert' in key:
-                new_key = key.replace('bert.', '')
-                state_dict[new_key] = state_dict[key]
-                del state_dict[key]
-            if 'beit' in key:
-                new_key = key.replace('beit.', '')
-                state_dict[new_key] = state_dict[key]
-                del state_dict[key]
+        m_pos_embed_reshaped = interpolate_pos_embed(state_dict['visual_encoder_m.pos_embed'], model.visual_encoder_m)
+        state_dict['visual_encoder_m.pos_embed'] = m_pos_embed_reshaped
 
         msg = model.load_state_dict(state_dict, strict=False)
         print('load checkpoint from %s' % args.checkpoint)
@@ -248,15 +232,10 @@ def main(args, config):
     model = model.to(device)
     model_without_ddp = model
 
-
     start_time = time.time()
-
-    score_val_i2t, score_val_t2i,labels_val = evaluate(model_without_ddp, val_loader, tokenizer, device, config)
-    score_test_i2t, score_test_t2i,labels_test = evaluate(model_without_ddp, test_loader, tokenizer, device, config)
-    print('VALIDATION')
-    val_stats = itm_eval(score_val_i2t,score_val_t2i,labels_val)
+    score_test_i2t, score_test_t2i, labels_test = evaluate(model_without_ddp, test_loader, tokenizer, device, config)
     print('TEST')
-    test_stats = itm_eval(score_test_i2t, score_test_t2i,labels_test)
+    itm_eval(score_test_i2t, score_test_t2i,labels_test)
 
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
@@ -265,9 +244,7 @@ def main(args, config):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--output_dir', default='./output/Retrieval')
-    #parser.add_argument('--checkpoint', default='')
-    parser.add_argument('--checkpoint', default='./Pretrain/checkpoint_15.pth')
+    parser.add_argument('--checkpoint', default='')
     parser.add_argument('--text_encoder', default='bert-base-uncased')
     parser.add_argument('--device', default='cuda')
     parser.add_argument('--seed', default=42, type=int)
@@ -276,8 +253,8 @@ if __name__ == '__main__':
     parser.add_argument('--distributed', default=False, type=bool)
     args = parser.parse_args()
 
-    cls_config = {
-        'image_res': 224,
+    ret_config = {
+        'image_res': 256,
         'batch_size_train': 4,
         'batch_size_test': 4,
         'alpha': 0.4,
@@ -286,12 +263,11 @@ if __name__ == '__main__':
         'vision_width': 768,
         'embed_dim': 256,
         'temp': 0.07,
+        'mlm_probability': 0.15,
         'k_test': 32,
         'distill':True,
         'warm_up':True,
         'bert_config': './config_bert.json',
     }
 
-    Path(args.output_dir).mkdir(parents=True, exist_ok=True)
-
-    main(args, cls_config)
+    main(args, ret_config)
